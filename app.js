248c87ef3ceeea91c7fd93bfdd9724796a4a7668
terx7 <erkileemet10@gmail.com>
2020-09-29 11:34:47 +0300

    .

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let gameBoard = [
 
 
 winningSums = [7, 56, 73, 84, 146, 273, 292, 448]
-var score = 0
+let score = 0
 
 const cellDivs = document.querySelectorAll('.cell')
 const scoreDiv = document.querySelectorAll('.score')
@@ -17,16 +17,14 @@ cellDivs.forEach(el => {
         const selectedCell = event.currentTarget
         const selectedRow = selectedCell.dataset.row
         const selectedCol = selectedCell.dataset.col
-        var cellValue = parseInt(selectedCell.dataset.value)
-        selectedCell.innerText = 'X'
-        score += cellValue
 
-
-        console.log(score)
 
         if (gameBoard[selectedRow][selectedCol] == "-") {
             selectedCell.innerText = 'X'
+            let cellValue = parseInt(selectedCell.dataset.value)
 
+            score += cellValue
+            console.log(score)
             gameBoard[selectedRow][selectedCol] = "X"
             console.log(gameBoard)
 
